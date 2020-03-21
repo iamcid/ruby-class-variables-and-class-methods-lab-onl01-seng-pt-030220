a14a1e6dfea690ef4f8fb79426b43f8f473ad385
Christopher Cid <iamcid17@gmail.com>
2020-03-21 22:15:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,4 +38,15 @@ class Song
     genre_count
   end
     
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
 end
