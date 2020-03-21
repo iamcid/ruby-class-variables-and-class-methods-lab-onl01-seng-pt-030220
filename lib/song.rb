96a7d46fb2e5f21aeac67914b9597f63d6c08223
Christopher Cid <iamcid17@gmail.com>
2020-03-21 21:59:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,8 +30,10 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
-        genre_count += 1
+        genre_count[genre] += 1
       else
-        genre_count = 1
+        genre_count[genre] = 1
+      end
+  end
     
 end
